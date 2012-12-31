2d65f6b0858f30fae5197db0ada995b0bb2ad897
Bruno Azevedo <brunodea@inf.ufsm.br>
2012-12-30 23:38:34 -0200

    adicionei novos contrutores para o TweetDAO e UserDAO.
    Por exemplo, se for salvar um novo usuário, só fazer UserDAO(user_previamente_preenchido).save();
    se não fizesse assim, os dados salvos seriam sempre errados.

--- src/br/ufsm/dsweb/dao/TweetDAO.java
+++ src/br/ufsm/dsweb/dao/TweetDAO.java
@@ -6,4 +6,7 @@ public class TweetDAO extends ModelDAO<Tweet> {
 	public TweetDAO() {
 		super(new Tweet(), "tweets.csv");
 	}
+	public TweetDAO(Tweet tweet) {
+		super(tweet, "tweets.csv");
+	}
 }
--- src/br/ufsm/dsweb/dao/UserDAO.java
+++ src/br/ufsm/dsweb/dao/UserDAO.java
@@ -7,6 +7,9 @@ public class UserDAO extends ModelDAO<User> {
 	public UserDAO() {
 		super(new User(), "users.csv");
 	}
+	public UserDAO(User user) {
+		super(user, "users.csv");
+	}
 
 	public boolean usernameExists(String username) {
 		String r = DBCore.getRowByCol(getFilename(), 2, username); 
