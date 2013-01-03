c4f119fc624c51bf39c1e3d2cd30d26d66914005
Bruno Azevedo <brunodea@inf.ufsm.br>
2013-01-03 20:37:21 -0200

    lista de tweets organizadas pela ordem da sua data de publicação.

--- src/br/ufsm/dsweb/dao/UserDAO.java
+++ src/br/ufsm/dsweb/dao/UserDAO.java
@@ -1,6 +1,9 @@
 package br.ufsm.dsweb.dao;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 
 import br.ufsm.dsweb.db.DBCore;
 import br.ufsm.dsweb.model.Tweet;
@@ -31,12 +34,19 @@ public class UserDAO extends ModelDAO<User> {
 	
 	public ArrayList<Tweet> getAllTweets(User user) {
 		ArrayList<Tweet> tweets = new ArrayList<Tweet>();
-		for(String csv : DBCore.getAllRowsByCol(getFilename(), 1, user.getID()+"")) {
+		for(String csv : DBCore.getAllRowsByCol(new TweetDAO().getFilename(), 1, user.getID()+"")) {
 			Tweet tweet = new Tweet();
 			tweet.fromCSV(csv);
 			
 			tweets.add(tweet);
 		}
+		
+		Collections.sort(tweets, new Comparator<Tweet>() {
+			@Override
+			public int compare(Tweet lhs, Tweet rhs) {
+				return rhs.getPubdate().compareTo(lhs.getPubdate());
+			}
+		});
 
 		return tweets;
 	}
