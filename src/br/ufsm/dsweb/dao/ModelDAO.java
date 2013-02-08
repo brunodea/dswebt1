6c7481acfbdac7e35bf84cff60b0d428de886552
Bruno Azevedo <brunodea@inf.ufsm.br>
2013-02-07 22:52:06 -0200

    tirei todos DBCore.algumacoisa() do ModelDAO.

--- src/br/ufsm/dsweb/dao/ModelDAO.java
+++ src/br/ufsm/dsweb/dao/ModelDAO.java
@@ -1,86 +1,80 @@
 package br.ufsm.dsweb.dao;
 
-import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
-import br.ufsm.dsweb.db.DBCore;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
 import br.ufsm.dsweb.model.Model;
 
+@Stateless
 public abstract class ModelDAO<T extends Model> {
 	private T mModel;
-	private String mFilename;
+	private String mTableName;
+	
+	@PersistenceContext(unitName="entitymanagertwitcher")
+	private EntityManager mEntityManager;
 
-	public ModelDAO(T model, String filename) {
+	public ModelDAO(T model, String tablename) {
 		mModel = model;
-		mFilename = filename;
+		mTableName = tablename;
+	}
+	
+	public EntityManager getEntityManager() {
+		return mEntityManager;
 	}
 
+	@SuppressWarnings("unchecked")
 	public T getByID(int model_id) {
 		T res = null;
-		String csv = DBCore.getRowByCol(mFilename, 0, String.valueOf(model_id));
-		if(!csv.equals("")) {
-			mModel.fromCSV(csv);
-			res = mModel;
+		try {
+			res = (T) getEntityManager().find(mModel.getClass(), model_id);
+		} catch(Exception e) {
 		}
 		return res;
 	}
 	public void save(T model) {
-		model.setID(getLastID()+1);
-		DBCore.appendToFile(mFilename, model.toCSV());
+		getEntityManager().persist(model);
 	}
 	public void removeByID(int model_id) {
-		DBCore.removeRowByCol(mFilename, 0, model_id+"");
+		T model = getByID(model_id);
+		if(model != null) {
+			getEntityManager().remove(model);
+		}
 	}
-	public void removeByCols(HashMap<Integer,String> col_vals) {
-		DBCore.removeRowByCols(mFilename, col_vals);
+	@SuppressWarnings("unchecked")
+	public void removeByCols(HashMap<String,String> col_vals) {
+		String query = "SELECT m FROM "+getTableName()+" m WHERE ";
+		for(String col : col_vals.keySet()) {
+			query += "m."+col+" = :"+col;
+		}
+		Query q = getEntityManager().createQuery(query);
+		for(String col : col_vals.keySet()) {
+			q.setParameter(col, col_vals.get(col));
+		}
+		for(T model : (List<T>) q.getResultList()) {
+			getEntityManager().remove(model);
+		}
 	}
 	public void update(T model) {
-		DBCore.updateRowByCol(mFilename, model.toCSV(), 0, model.getID()+"");
+		getEntityManager().merge(model);
 	}
-	public ArrayList<T> getAllByCol(int col, String value) {
-		ArrayList<T> list = new ArrayList<T>();
-		for(String csv : DBCore.getAllRowsByCol(getFilename(), col, value)) {
-			try {
-				T model = (T)mModel.getClass().newInstance();
-				model.fromCSV(csv);
-				list.add(model);
-			} catch (InstantiationException e) {
-				e.printStackTrace();
-			} catch (IllegalAccessException e) {
-				e.printStackTrace();
-			}
-		}
-		return list;
+	@SuppressWarnings("unchecked")
+	public List<T> getAllByCol(String col, String value) {
+		return (List<T>) getEntityManager().createQuery("SELECT m FROM " +getTableName()+ " m WHERE m."+col+" = :"+col)
+				.setParameter(col, value)
+				.getResultList();
 	}
-	public ArrayList<T> getAll() {
-		ArrayList<T> list = new ArrayList<T>();
-		for(String csv : DBCore.getAllRows(getFilename())) {
-			try {
-				T model = (T)mModel.getClass().newInstance();
-				model.fromCSV(csv);
-				list.add(model);
-			} catch (InstantiationException e) {
-				e.printStackTrace();
-			} catch (IllegalAccessException e) {
-				e.printStackTrace();
-			}
-		}
-		return list;		
+	@SuppressWarnings("unchecked")
+	public List<T> getAll() {
+		return (List<T>) getEntityManager().createQuery("SELECE m FROM " +getTableName())
+				.getResultList();
 	}
 	
-	public String getFilename() {
-		return mFilename;
-	}
-
-	private int getLastID() {
-		int last_id = -1;
-		
-		String last_row = DBCore.getLastRow(mFilename);
-		if(!last_row.equals("")) {
-			String[] vals = last_row.split(DBCore.SEPARATOR);
-			last_id = Integer.parseInt(vals[0]);
-		}
-		
-		return last_id;
+	public String getTableName() {
+		return mTableName;
 	}
 }
