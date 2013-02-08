192c79db1534032d7786ff48dce75af8122d804f
Bruno Azevedo <brunodea@inf.ufsm.br>
2013-02-07 23:22:04 -0200

    adicionei alguns métodos no ModelDAO.

--- src/br/ufsm/dsweb/dao/ModelDAO.java
+++ src/br/ufsm/dsweb/dao/ModelDAO.java
@@ -42,9 +42,9 @@ public abstract class ModelDAO<T extends Model> {
 	public T getFirstByCol(String col, String value) {
 		T res = null;
 		try {
-		 res = (T) getEntityManager().createQuery("SELECT m FROM " +getTableName()+" m WHERE m."+col+" = :"+col)
-				 .setParameter(col, value)
-				 .getSingleResult();
+			res = (T) getEntityManager().createQuery("SELECT m FROM " +getTableName()+" m WHERE m."+col+" = :"+col)
+					.setParameter(col, value)
+					.getSingleResult();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
@@ -52,6 +52,16 @@ public abstract class ModelDAO<T extends Model> {
 	}
 	public T getFirstByCol(String col, int value) {
 		return getFirstByCol(col, value+"");
+	}
+	@SuppressWarnings("unchecked")
+	public T getFirstByCols(HashMap<String, String> col_vals) {
+		T res = null;
+		try {
+			res = (T)createQuery(col_vals).getSingleResult();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return res;
 	}
 	public void save(T model) {
 		try {
@@ -72,16 +82,8 @@ public abstract class ModelDAO<T extends Model> {
 	}
 	@SuppressWarnings("unchecked")
 	public void removeByCols(HashMap<String,String> col_vals) {
-		String query = "SELECT m FROM "+getTableName()+" m WHERE ";
-		for(String col : col_vals.keySet()) {
-			query += "m."+col+" = :"+col;
-		}
-		Query q = getEntityManager().createQuery(query);
-		for(String col : col_vals.keySet()) {
-			q.setParameter(col, col_vals.get(col));
-		}
 		try {
-			for(T model : (List<T>) q.getResultList()) {
+			for(T model : (List<T>) createQuery(col_vals).getResultList()) {
 				getEntityManager().remove(model);
 			}
 		} catch(Exception e) {
@@ -111,6 +113,17 @@ public abstract class ModelDAO<T extends Model> {
 		return getAllByCol(col, value+"");
 	}
 	@SuppressWarnings("unchecked")
+	public List<T> getAllByCols(HashMap<String, String> col_vals) {
+		List<T> res = new ArrayList<T>();
+		try {
+			res = (List<T>) createQuery(col_vals).getResultList();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		return res;
+	}
+	@SuppressWarnings("unchecked")
 	public List<T> getAll() {
 		List<T> res = new ArrayList<T>();
 		try {
@@ -121,7 +134,19 @@ public abstract class ModelDAO<T extends Model> {
 		}
 		return res;
 	}
-
+	private Query createQuery(HashMap<String, String> col_vals) {
+		String query = "SELECT m FROM "+getTableName()+" m WHERE ";
+		for(String col : col_vals.keySet()) {
+			query += "m."+col+" = :"+col;
+		}
+		Query q = getEntityManager().createQuery(query);
+		for(String col : col_vals.keySet()) {
+			q.setParameter(col, col_vals.get(col));
+		}
+		
+		return q;
+	}
+	
 	public String getTableName() {
 		return mTableName;
 	}
